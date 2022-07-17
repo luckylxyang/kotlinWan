5063a3a61824d2733d35f47cd54b686c0a3513aa
luckylxyang <liuxy0528@gmail.com>
2022-07-17 16:35:14 +0800

    login ui

--- app/src/main/java/com/lxy/kotlinwan/network/ApiService.kt
+++ app/src/main/java/com/lxy/kotlinwan/network/ApiService.kt
@@ -1,12 +1,16 @@
 package com.lxy.kotlinwan.network
 
-import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.*
 
 interface ApiService {
 
+    @POST("user/login")
+    @FormUrlEncoded
+    suspend fun login(@Field("username") username : String, @Field("password") password :String)
 
-
+    /**
+     * 获取文章列表
+     */
     @GET("article/list/{pageIndex}/json")
     suspend fun getArticles(@Path("pageIndex") index: Int) : Resource<Data>
 }
